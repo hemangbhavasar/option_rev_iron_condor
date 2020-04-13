v0.43 *Fixed volume calculation error

rev_iron_condor.py
@@ -1,7 +1,10 @@
 """
 Rev iron condor analyser
 Written by: Peter Agalakov
-version: v0.42(2020-april-09)
+version: v0.43(2020-april-11)
+
+v0.43 (2020-april-11)
+*Fixed volume calculation error
 
 V0.42(2020-april-09)
 *Use last price instead of Mid
@@ -136,23 +139,24 @@ def get_min_price(p, c, s, legs):
     for i in range(len(leg1[2])):
         leg1_strike_price = leg1[2].loc[i, 'Strike']
         leg1_price = leg1[2].loc[i, 'Last Price']
-        volume += int(leg1[2].loc[i, 'Volume'])
+        leg1_volume = int(leg1[2].loc[i, 'Volume'])
         for l in range(len(leg2[2])):
             if leg1_strike_price + s[0] == leg2[2].loc[l, 'Strike']:
                 leg2_strike_price = leg2[2].loc[l, 'Strike']
                 leg2_price = leg2[2].loc[l, 'Last Price']
-                volume += int(leg2[2].loc[l, 'Volume'])
+                leg2_volume = int(leg2[2].loc[l, 'Volume'])
                 for m in range(len(leg3[2])):
-                    if leg1_strike_price + s[0] + s[1] == leg3[2].loc[m, 'Strike']:
+                    if leg1_strike_price + s[0] + s[1] == \
+                            leg3[2].loc[m, 'Strike']:
                         leg3_strike_price = leg3[2].loc[m, 'Strike']
                         leg3_price = leg3[2].loc[m, 'Last Price']
-                        volume += int(leg3[2].loc[m, 'Volume'])
+                        leg3_volume = int(leg3[2].loc[m, 'Volume'])
                         for n in range(len(leg4[2])):
                             if leg1_strike_price + s[0] + s[1] + s[2] == \
                                     leg4[2].loc[n, 'Strike']:
                                 leg4_strike_price = leg4[2].loc[n, 'Strike']
                                 leg4_price = leg4[2].loc[n, 'Last Price']
-                                volume = int(leg4[2].loc[n, 'Volume'])
+                                leg4_volume = int(leg4[2].loc[n, 'Volume'])
                                 value = gen_price_value(leg1_price,
                                                         leg2_price,
                                                         leg3_price,
@@ -163,14 +167,15 @@ def get_min_price(p, c, s, legs):
                                 leg2_list.append(leg2_strike_price)
                                 leg3_list.append(leg3_strike_price)
                                 leg4_list.append(leg4_strike_price)
-                                v.append(volume / 4)
+                                v.append((leg1_volume + leg2_volume +
+                                          leg3_volume + leg4_volume) / 4)
 
     return gen_price_slot, leg1_list, leg2_list, leg3_list, leg4_list, v
 
 
 def gen_price_value(l1_p, l2_p, l3_p, l4_p, legs):
     value = (legs[0][0] * l1_p) + (legs[1][0] * l2_p) + (legs[2][0] *
-                                                           l3_p) \
+                                                         l3_p) \
             + (legs[3][0] * l4_p)
     value = np.around(value, decimals=2)
     return value
@@ -205,11 +210,13 @@ def f_df_func(strategy, stock, exp_dates, legs):
                                                                    8:10])
         # Get put price DataFrame and filter it down to get the necessary
         # columns
-        puts_price = get_df(get_puts(stock, exp_date))
+        puts_price = get_puts(stock, exp_date)
+        puts_price = get_df(puts_price)
 
         # Get put price DataFrame and filter it down to get the necessary
         # columns
-        calls_price = get_df(get_calls(stock, exp_date))
+        calls_price = get_calls(stock, exp_date)
+        calls_price = get_df(calls_price)
 
         # Use get_min_price function to obtain a DataFrame with all the price
         # for each leg + Volume + Open interest
@@ -220,7 +227,7 @@ def f_df_func(strategy, stock, exp_dates, legs):
                 'Buy_calls': table4, 'Sell_calls': table5, 'Volume': table6}
 
         data_frame = pd.DataFrame(data)
-        data_frame = filter_volume(data_frame)
+        # data_frame = data_frame.reset_index()
         df2 = pd.DataFrame([data_frame['Cost'].max(), datetime.now()])
         df2 = df2.transpose()
         # append_df_to_excel('output.xlsx', data_frame,
@@ -230,9 +237,9 @@ def f_df_func(strategy, stock, exp_dates, legs):
         #                    index=False, header=None)
         print("Data was added to excel file for:" + stock + " @ " + str(
             datetime.now()))
-        print(df2)
+        print(data_frame.to_string(index=False))
         print("\n")
-        print(data_frame)
+        print(df2.to_string(index=False))
 
 
 # Setup-------------------------------------
@@ -261,12 +268,12 @@ variation = [2, 2, 2]
 exp_dates = ['2020/05/15']  # string format 'yyyy/mm/dd'
 
 """The time interval variable in minutes that the data is collected"""
-timer = 15  # in minutes
+timer = 1  # in minutes
 """THe days the stock market is open, if trading on only specific days of 
 the week this can be modified to ignore other days of the week."""
 open_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 
-volume_filter = 150
+volume_filter = 500
 
 """Buy or sell the current legs (1 = sell, -1 = buy)"""
 leg1 = [1, 'put']
@@ -278,8 +285,8 @@ legs = [leg1, leg2, leg3, leg4]
 # -----------------------------------------
 
 
-schedule.every(timer).minutes.do(f_df_func, strategy, stock, exp_dates,
-                                 legs)
+schedule.every(timer).minutes.do(f_df_func, strategy, stock,
+                                 exp_dates, legs)
 
 while True:
     now = datetime.now()
@@ -290,7 +297,7 @@ while True:
     # 57600 : 16h00
     if 34200 < s_s_m < 57600 and day in open_days:
         schedule.run_pending()
-        time.sleep(1)
+        time.sleep(600)
     else:
         print("Markets are now closed, the tracker is on stand-by ")
         time.sleep(900)