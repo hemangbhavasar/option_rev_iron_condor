33295567f13e5f240c5ff4aa4352ee69a7cbc769
Peter-AK <pioter26@hotmail.com>
2020-04-21 12:32:20 -0400

    v0.45 (2020-april-21) * Major Refactoring + OOP

--- rev_iron_condor.py
+++ rev_iron_condor.py
@@ -1,11 +1,13 @@
 """
 Rev iron condor analyser
 Written by: Peter Agalakov
-version: v0.44(2020-april-11)
+version: v0.45(2020-april-21)
+
+v0.45 (2020-april-21)
+* Major Refactoring + OOP
 
 v0.44 (2020-april-14)
 * Refactoring + OOP
-
 
 v0.432 (2020-april-11)
 * Fixed volume calculation error
@@ -45,82 +47,12 @@ from yahoo_fin.options import *
 from yahoo_fin.stock_info import *
 from datetime import datetime
 import time
-from openpyxl import load_workbook
 from settings import Settings, setup
-
-
-def append_df_to_excel(filename, df, sheet_name='Sheet1', startrow=None,
-                       truncate_sheet=False,
-                       **to_excel_kwargs):
-    """
-    Append a DataFrame [df] to existing Excel file [filename]
-    into [sheet_name] Sheet.
-    If [filename] doesn't exist, then this function will create it.
-
-    Parameters:
-      filename : File path or existing ExcelWriter
-                 (Example: '/path/to/file.xlsx')
-      df : dataframe to save to workbook
-      sheet_name : Name of sheet which will contain DataFrame.
-                   (default: 'Sheet1')
-      startrow : upper left cell row to dump data frame.
-                 Per default (startrow=None) calculate the last row
-                 in the existing DF and write to the next row...
-      truncate_sheet : truncate (remove and recreate) [sheet_name]
-                       before writing DataFrame to Excel file
-      to_excel_kwargs : arguments which will be passed to `DataFrame.to_excel()`
-                        [can be dictionary]
-
-    Returns: None
-    """
-    # ignore [engine] parameter if it was passed
-    if 'engine' in to_excel_kwargs:
-        to_excel_kwargs.pop('engine')
-
-    writer = pd.ExcelWriter(filename, engine='openpyxl')
-
-    # Python 2.x: define [FileNotFoundError] exception if it doesn't exist
-    try:
-        FileNotFoundError
-    except NameError:
-        FileNotFoundError = IOError
-
-    try:
-        # try to open an existing workbook
-        writer.book = load_workbook(filename)
-
-        # get the last row in the existing Excel sheet
-        # if it was not specified explicitly
-        if startrow is None and sheet_name in writer.book.sheetnames:
-            startrow = writer.book[sheet_name].max_row
-
-        # truncate sheet
-        if truncate_sheet and sheet_name in writer.book.sheetnames:
-            # index of [sheet_name] sheet
-            idx = writer.book.sheetnames.index(sheet_name)
-            # remove [sheet_name]
-            writer.book.remove(writer.book.worksheets[idx])
-            # create an empty sheet [sheet_name] using old index
-            writer.book.create_sheet(sheet_name, idx)
-
-        # copy existing sheets
-        writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
-    except FileNotFoundError:
-        # file does not exist yet, we will create it
-        pass
-
-    if startrow is None:
-        startrow = 0
-
-    # write out the new sheet
-    df.to_excel(writer, sheet_name, startrow=startrow, **to_excel_kwargs)
-
-    # save the workbook
-    writer.save()
+from append_df_to_excel import append_df_to_excel
 
 
 # noinspection PyUnresolvedReferences
-def get_min_price(p, c, s, legs):
+def get_min_price(log):
     """4 loops nested in each other, first loop selects the items down the
     dataframe list. The other loops verify that the strategy suggested is
     possible since not all strike prices have the necessary volume. Finally
@@ -135,38 +67,33 @@ def get_min_price(p, c, s, legs):
     leg4_list = []
     v = []
 
-    for leg in legs:
-        if leg[1] == 'put':
-            leg.append(p)
-        elif leg[1] == 'call':
-            leg.append(c)
-
-    for i in range(len(leg1[2])):
-        leg1_strike_price = leg1[2].loc[i, 'Strike']
-        leg1_price = leg1[2].loc[i, 'Last Price']
-        leg1_volume = int(leg1[2].loc[i, 'Volume'])
-        for l in range(len(leg2[2])):
-            if leg1_strike_price + s[0] == leg2[2].loc[l, 'Strike']:
-                leg2_strike_price = leg2[2].loc[l, 'Strike']
-                leg2_price = leg2[2].loc[l, 'Last Price']
-                leg2_volume = int(leg2[2].loc[l, 'Volume'])
-                for m in range(len(leg3[2])):
-                    if leg1_strike_price + s[0] + s[1] == \
-                            leg3[2].loc[m, 'Strike']:
-                        leg3_strike_price = leg3[2].loc[m, 'Strike']
-                        leg3_price = leg3[2].loc[m, 'Last Price']
-                        leg3_volume = int(leg3[2].loc[m, 'Volume'])
-                        for n in range(len(leg4[2])):
-                            if leg1_strike_price + s[0] + s[1] + s[2] == \
-                                    leg4[2].loc[n, 'Strike']:
-                                leg4_strike_price = leg4[2].loc[n, 'Strike']
-                                leg4_price = leg4[2].loc[n, 'Last Price']
-                                leg4_volume = int(leg4[2].loc[n, 'Volume'])
+    for i in range(len(log.leg1)):
+        leg1_strike_price = log.leg1.loc[i, 'Strike']
+        leg1_price = log.leg1.loc[i, 'Last Price']
+        leg1_volume = int(log.leg1.loc[i, 'Volume'])
+        for l in range(len(log.leg2)):
+            if leg1_strike_price + log.strategy[1] == log.leg2.loc[l,
+                                                                   'Strike']:
+                leg2_strike_price = log.leg2.loc[l, 'Strike']
+                leg2_price = log.leg2.loc[l, 'Last Price']
+                leg2_volume = int(log.leg2.loc[l, 'Volume'])
+                for m in range(len(log.leg3)):
+                    if leg1_strike_price + log.strategy[2] == \
+                            log.leg3.loc[m, 'Strike']:
+                        leg3_strike_price = log.leg3.loc[m, 'Strike']
+                        leg3_price = log.leg3.loc[m, 'Last Price']
+                        leg3_volume = int(log.leg3.loc[m, 'Volume'])
+                        for n in range(len(log.leg4)):
+                            if leg1_strike_price + log.strategy[3] == \
+                                    log.leg4.loc[n, 'Strike']:
+                                leg4_strike_price = log.leg4.loc[n, 'Strike']
+                                leg4_price = log.leg4.loc[n, 'Last Price']
+                                leg4_volume = int(log.leg4.loc[n, 'Volume'])
                                 value = gen_price_value(leg1_price,
                                                         leg2_price,
                                                         leg3_price,
                                                         leg4_price,
-                                                        legs)
+                                                        log)
                                 gen_price_slot.append(value)
                                 leg1_list.append(leg1_strike_price)
                                 leg2_list.append(leg2_strike_price)
@@ -174,79 +101,47 @@ def get_min_price(p, c, s, legs):
                                 leg4_list.append(leg4_strike_price)
                                 v.append((leg1_volume + leg2_volume +
                                           leg3_volume + leg4_volume) / 4)
+                                continue
 
     return gen_price_slot, leg1_list, leg2_list, leg3_list, leg4_list, v
 
 
-def gen_price_value(l1_p, l2_p, l3_p, l4_p, legs):
-    value = (legs[0][0] * l1_p) + (legs[1][0] * l2_p) + \
-            (legs[2][0] * l3_p) + (legs[3][0] * l4_p)
+def gen_price_value(l1_p, l2_p, l3_p, l4_p, log):
+    value = (log.leg1_sell * l1_p) + (log.leg2_sell * l2_p) + \
+            (log.leg3_sell * l3_p) + (log.leg4_sell * l4_p)
     value = np.around(value, decimals=2)
     return value
 
 
-def filter_blank(df, col):
-    """Filter empty spaces"""
-    df_filter = df[col] != '-'
-    return df[df_filter]
+def f_df_func(log):
+    sheet_name = str(log.exp_date[0:4]) + str(log.exp_date[5:7]) + \
+                 str(log.exp_date[8:10])
+
+    # Use get_min_price function to obtain a DataFrame with all the price
+    # for each leg + Volume
+    table1, table2, table3, table4, table5, table6 = \
+        get_min_price(log)
+
+    data = {'Cost': table1, 'Sell_puts': table2, 'Buy_puts': table3,
+            'Buy_calls': table4, 'Sell_calls': table5, 'Volume': table6}
+
+    data_frame = pd.DataFrame(data)
+    # data_frame = data_frame.reset_index()
+    df2 = pd.DataFrame([data_frame['Cost'].max(), datetime.now()])
+    df2 = df2.transpose()
+    append_df_to_excel('output.xlsx', data_frame,
+                       sheet_name=log.stock + sheet_name)
+    append_df_to_excel('output.xlsx', df2,
+                       sheet_name=log.stock + sheet_name + "_Min_cost",
+                       index=False, header=None)
+    print("Data was added to excel file for:" + log.stock + " @ " + str(
+        datetime.now()))
+    # print(data_frame.to_string(index=False))
+    # print("\n")
+    # print(df2.to_string(index=False))
 
 
-def filter_volume(df):
-    """Filter the data frame by volume"""
-    global volume_filter
-    vol_filter = pd.to_numeric(df['Volume']) > volume_filter
-    return df[vol_filter]
-
-
-def get_df(o_df):
-    """ Function that filters, converts and rearranges the original
-    data frame"""
-    o_df = o_df[['Strike', 'Last Price', 'Volume']]
-    o_df = filter_blank(o_df, 'Volume')
-    o_df = filter_volume(o_df)
-    o_df = o_df.reset_index()
-    return o_df
-
-
-def f_df_func(strategy, stock, exp_dates, legs):
-    for exp_date in exp_dates:
-        sheet_name = str(exp_date[0:4]) + str(exp_date[5:7]) + str(exp_date[
-                                                                   8:10])
-        # Get put price DataFrame and filter it down to get the necessary
-        # columns
-        puts_price = get_puts(stock, exp_date)
-        puts_price = get_df(puts_price)
-
-        # Get put price DataFrame and filter it down to get the necessary
-        # columns
-        calls_price = get_calls(stock, exp_date)
-        calls_price = get_df(calls_price)
-
-        # Use get_min_price function to obtain a DataFrame with all the price
-        # for each leg + Volume
-        table1, table2, table3, table4, table5, table6 = get_min_price(
-            puts_price, calls_price, strategy, legs)
-
-        data = {'Cost': table1, 'Sell_puts': table2, 'Buy_puts': table3,
-                'Buy_calls': table4, 'Sell_calls': table5, 'Volume': table6}
-
-        data_frame = pd.DataFrame(data)
-        # data_frame = data_frame.reset_index()
-        df2 = pd.DataFrame([data_frame['Cost'].max(), datetime.now()])
-        df2 = df2.transpose()
-        # append_df_to_excel('output.xlsx', data_frame,
-        #                    sheet_name=stock + sheet_name)
-        # append_df_to_excel('output.xlsx', df2,
-        #                    sheet_name=stock + sheet_name + "_Min_cost",
-        #                    index=False, header=None)
-        print("Data was added to excel file for:" + stock + " @ " + str(
-            datetime.now()))
-        print(data_frame.to_string(index=False))
-        print("\n")
-        print(df2.to_string(index=False))
-
-
-def scan(strategy, stock, exp_dates, legs, timer, open_days):
+def scan(log):
     while True:
         now = datetime.now()
         day = now.strftime("%A")
@@ -254,27 +149,27 @@ def scan(strategy, stock, exp_dates, legs, timer, open_days):
                                    microsecond=0)).total_seconds()
         # 34200 : 09h30
         # 57600 : 16h00
-        if 34200 < s_s_m < 57600 and day in open_days:
-            f_df_func(strategy, stock, exp_dates, legs)
-            time.sleep(timer)
+        if 34200 < s_s_m < 57600 and day in log.open_days:
+            Settings.get_call_put_lists(log)
+            Settings.leg_assignments(log, log.legs)
+            f_df_func(log)
+            time.sleep(log.timer)
         else:
             print("Markets are now closed, the tracker is on stand-by ")
             time.sleep(900)
+# ---------------------------------------------------------------------
 
 
 setup()
+legs = [['Put', 'Sell', 2], ['Put', 'Buy', 2], ['Call', 'Buy', 2], ['Call', 'Sell', 2]]
+log1 = Settings(stock='spy',
+                strategy=['5', '10', '5'],
+                variation=['2', '2', '2'],
+                exp_date='2020/05/15',
+                timer=600,
+                legs=legs,
+                volume_filter=500)
 
-log1 = Settings()
-
-
-"""Buy or sell the current legs (1 = sell, -1 = buy)"""
-leg1 = [1, 'put']
-leg2 = [-1, 'put']
-leg3 = [-1, 'call']
-leg4 = [1, 'call']
-
-legs = [leg1, leg2, leg3, leg4]
 # -----------------------------------------
 
-scan(log1.strategy, log1.stock, log1.exp_dates, legs, log1.timer,
-     log1.open_days)
+scan(log1)
--- settings.py
+++ settings.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pandas as pd
+from yahoo_fin.options import *
+from yahoo_fin.stock_info import *
 
 
 def setup():
+    """Parameters for pandas and scientific notation in lists"""
     # Display all rows and columns from DataFrame
     pd.set_option('display.max_columns', None)
     pd.set_option("max_rows", None)
@@ -10,23 +13,115 @@ def setup():
     np.set_printoptions(suppress=True)
 
 
+def call_put_lists(stock, exp_date):
+    # Get put price DataFrame and filter it down to get the necessary
+    # columns
+    puts_list = get_puts(stock, exp_date)
+    # puts_price = get_df(puts_price)
+
+    # Get put price DataFrame and filter it down to get the necessary
+    # columns
+    calls_list = get_calls(stock, exp_date)
+    # calls_price = get_df(calls_price)
+    return puts_list, calls_list
+
+
+def filter_blank(df, col):
+    """Filter empty spaces"""
+    df_filter = df[col] != '-'
+    return df[df_filter]
+
+
+def converted_strat(strategy):
+    """Converts a spread strategy into a list that can be used by the
+    the code later on"""
+    s1 = 0
+    s2 = int(strategy[0])
+    s3 = int(strategy[0]) + int(strategy[1])
+    s4 = int(strategy[0]) + int(strategy[1]) + int(strategy[2])
+    return [s1, s2, s3, s4]
+
+
+def assign_leg_df(leg, p, c):
+    """Function that will be run after creating an initial log,
+    to assign each legs attributes"""
+    if leg[0] == 'Put':
+        leg = p
+    elif leg[0] == 'Call':
+        leg = c
+    return leg
+
+
+def assign_leg_sell(leg):
+    """Crate the sell or buy variables for each leg (1 means that you
+        will sell the leg and there for gain money, -1 is to buy and there
+        fore lose money)"""
+    if leg[1] == 'Sell':
+        return 1
+    if leg[1] == 'Buy':
+        return -1
+    else:
+        return 0
+
+
 class Settings:
     """Initialize all the settings here"""
-    def __init__(self):
+    def __init__(self, stock, strategy, variation, exp_date, timer, legs,
+                 volume_filter):
         # The ticket or the underlying stock
-        self.stock = "spy"
+        self.stock = stock
         # The interval desired for each leg of the iron condor in dollars.
         # ex: 10/20/10 -> 240/250/270/280 and it's variation (not implemented)
-        self.strategy = [5, 10, 5]
-        self.variation = [2, 2, 2]
+        self.strategy = converted_strat(strategy)
+        self.variation = variation
+        self.legs = legs
         # Enter the list of expiration dates for contracts
         # (string format 'yyyy/mm/dd')
-        self.exp_dates = ['2020/05/15']
+        self.exp_date = exp_date
         # The time interval variable in minutes that the data is collected
         # timer in seconds
-        self.timer = 600
+        self.timer = timer
         self.open_days = ['Monday', 'Tuesday', 'Wednesday',  'Thursday',
                           'Friday']
         # The minimum volume for each option to be collected.
-        self.volume_filter = 500
+        self.volume_filter = volume_filter
+        self.put_list = None
+        self.call_list = None
+        # self.get_call_put_lists()
+        # Create the DataFrames for future use in filling in put and call
+        # tables + sell/buy
+        self.leg1 = None
+        self.leg2 = None
+        self.leg3 = None
+        self.leg4 = None
 
+        self.leg1_sell = assign_leg_sell(legs[0])
+        self.leg2_sell = assign_leg_sell(legs[1])
+        self.leg3_sell = assign_leg_sell(legs[2])
+        self.leg4_sell = assign_leg_sell(legs[3])
+
+    def filter_volume(self, df):
+        """Filter the data frame by volume"""
+        vol_filter = pd.to_numeric(df['Volume']) > self.volume_filter
+        return df[vol_filter]
+
+    def get_df(self, o_df):
+        """ Function that filters, converts and rearranges the original
+        data frame"""
+        o_df = o_df[['Strike', 'Last Price', 'Volume']]
+        o_df = filter_blank(o_df, 'Volume')
+        o_df = self.filter_volume(o_df)
+        o_df = o_df.reset_index()
+        return o_df
+
+    def get_call_put_lists(self):
+        self.put_list, self.call_list = call_put_lists(self.stock,
+                                                      self.exp_date)
+        self.put_list = self.get_df(self.put_list)
+        self.call_list = self.get_df(self.call_list)
+
+    def leg_assignments(self, legs):
+        self.leg1 = assign_leg_df(legs[0], self.put_list, self.call_list)
+        self.leg2 = assign_leg_df(legs[1], self.put_list, self.call_list)
+        self.leg3 = assign_leg_df(legs[2], self.put_list, self.call_list)
+        self.leg4 = assign_leg_df(legs[3], self.put_list, self.call_list)
